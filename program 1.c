b53060712b67ef2608ed565c5bebbc4097a1f345
AdharshAmar <150884500+AdharshAmar@users.noreply.github.com>
2023-11-28 22:20:01 +0530

    Update program 1.c

--- program 1.c
+++ program 1.c
@@ -1,76 +1,56 @@
 #include <stdio.h>
-
-// Function to perform union of two sets
-void unionSets(int set1[], int set2[], int result[], int size) {
-    for (int i = 0; i < size; i++) {
-        result[i] = set1[i] | set2[i]; // Union is achieved through bitwise OR
-    }
+int main(){
+int n1,n2;
+printf("Enter the size of the first array: ");
+scanf("%d",&n1);
+int a[n1];
+printf("Enter the array elements in sorted order: ");
+for(int i=0;i<n1;i++){
+scanf("%d",&a[i]);
 }
 
-// Function to perform intersection of two sets
-void intersectSets(int set1[], int set2[], int result[], int size) {
-    for (int i = 0; i < size; i++) {
-        result[i] = set1[i] & set2[i]; // Intersection is achieved through bitwise AND
-    }
+printf("Enter the size of the second array: ");
+scanf("%d",&n2);
+int b[n2];
+printf("Enter the array elements in sorted order: ");
+for(int i=0;i<n2;i++){
+scanf("%d",&b[i]);
 }
 
-// Function to perform difference of two sets (set1 - set2)
-void differenceSets(int set1[], int set2[], int result[], int size) {
-    for (int i = 0; i < size; i++) {
-        result[i] = set1[i] & (~set2[i]); // Difference is achieved through bitwise AND and negation
-    }
+void merge (int n1,int n2,int a[],int b[])
+{
+int i=0,j=0,k=0;
+int s[n1+n2];
+while(i<n1 && j<n2){
+if(a[i]<=b[j])
+{
+s[k]=a[i];
+k++;
+i++;
+}else{
+s[k]=b[j];
+k++;
+j++;
+
+}
 }
 
-// Function to display the elements of a set
-void displaySet(int set[], int size) {
-    printf("{ ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", set[i]);
-    }
-    printf("}\n");
+while(n1>i){
+s[k]=a[i];
+k++;
+i++;
 }
 
-int main() {
-    int size;
+while(n2>j){
+s[k]=b[j];
+k++;
+j++;
+}
+for(i=0;i<n1+n2;i++){
+printf("%d ",s[i]);
+}
+}
 
-    // Input size of the sets
-    printf("Enter the size of the sets: ");
-    scanf("%d", &size);
+merge(n1,n2,a,b);
 
-    int set1[size], set2[size], resultSet[size];
-
-    // Input values for Set 1
-    printf("Enter elements for Set 1:\n");
-    for (int i = 0; i < size; i++) {
-        printf("Enter element %d: ", i + 1);
-        scanf("%d", &set1[i]);
-    }
-
-    // Input values for Set 2
-    printf("Enter elements for Set 2:\n");
-    for (int i = 0; i < size; i++) {
-        printf("Enter element %d: ", i + 1);
-        scanf("%d", &set2[i]);
-    }
-
-    // Display sets
-    printf("\nSet 1: ");
-    displaySet(set1, size);
-    printf("Set 2: ");
-    displaySet(set2, size);
-
-    // Perform set operations
-    unionSets(set1, set2, resultSet, size);
-    printf("\nUnion: ");
-    displaySet(resultSet, size);
-
-    intersectSets(set1, set2, resultSet, size);
-    printf("Intersection: ");
-    displaySet(resultSet, size);
-
-    differenceSets(set1, set2, resultSet, size);
-    printf("Difference (Set 1 - Set 2): ");
-    displaySet(resultSet, size);
-
-    return 0;
 }
